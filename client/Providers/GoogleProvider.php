8c2b9a4f38b1b2a33c851f07eaa8a0ecf24df54f
Dounia HARAG <59885752+douniaharag@users.noreply.github.com>
2022-07-17 21:47:29 +0200

    🔧 Update and Fix Google Provider 🔧

--- client/Providers/GoogleProvider.php
+++ client/Providers/GoogleProvider.php
@@ -2,52 +2,66 @@
 
 class GoogleProvider
 {
-    public static string $clientId = '122194665762-h246aajbs1sqfklna28192mr03red1sk.apps.googleusercontent.com';
-    private static string $clientSecret = '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';
+
+    public static string $clientId = '853608540318-iu1uqhv9q198bqebva35ms052p0bvb3t.apps.googleusercontent.com';
+    private static string $clientSecret = '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';
 
 
     public static function callback()
     {
-            $specifParams = [
-                    "grant_type" => "authorization_code",
-                    "code" => $_GET["code"],
-                ];
-            $clientId = "122194665762-h246aajbs1sqfklna28192mr03red1sk.apps.googleusercontent.com";
-            $clientSecret = "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";
-            $redirectUri = "http://localhost:8081/gg_callback";
-            $data = http_build_query(array_merge([
-                "redirect_uri" => $redirectUri,
-                "client_id" => $clientId,
-                "client_secret" => $clientSecret
-            ], $specifParams));
+        $token = GoogleProvider::getToken("https://oauth2.googleapis.com/token", GoogleProvider::$clientId, GoogleProvider::$clientSecret);
+        $user = GoogleProvider::getUser($token);
+        echo "<pre>";
+        var_dump($token);
+        var_dump($user);
+        echo "</pre>";
+    }
 
-            $url = "https://oauth2.googleapis.com/token";
-            $options = array(
-                'http' => array(
-                    'method' => 'POST',
-                    'header' => 'Content-Type: application/x-www-form-urlencoded',
-                    'content' => $data
-                )
-            );
-            $context = stream_context_create($options);
-            $result = file_get_contents($url, false, $context);
-            $result = json_decode($result, true);
-            $accessToken = $result['access_token'];
-        
-            $url = "https://www.googleapis.com/oauth2/v1/userinfo";
-            $options = array(
-                'http' => array(
-                    'method' => 'GET',
-                    'header' => 'Authorization: Bearer ' . $accessToken
-                )
-            );
-            $context = stream_context_create($options);
-            $result = file_get_contents($url, false, $context);
-            $result = json_decode($result, true);
-            echo "Hello {$result['email']}
-            
+    private static function getToken($baseUrl, $clientId, $clientSecret)
+    {
+        ["code"=> $code, "state" => $state] = $_GET;
+        $queryParams = http_build_query([
+            "client_id"=> $clientId,
+            "client_secret"=> $clientSecret,
+            "redirect_uri"=>"https://localhost/gg_oauth_success",
+            "code"=> $code,
+            "grant_type"=>"authorization_code",
+        ]);
+
+        $context = stream_context_create([
+            "http"=>[
+                "method"=>"POST",
+                "header"=>"Accept: application/x-www-form-urlencoded",
+                "content"=>$queryParams,
+            ]
+        ]);
+
+        $url = $baseUrl . "?{$queryParams}";
+        $response = file_get_contents($url, false, $context);
+        if (!$response) {
+            echo $http_response_header;
+            return;
         }
+        ["access_token" => $token] = json_decode($response, true);
 
-    
+        return $token;
+        var_dump($token);
+    }
 
+    private static function getUser($token)
+    {
+        $context = stream_context_create([
+            "http"=>[
+                "header"=>"Authorization: Bearer {$token}"
+            ]
+        ]);
+
+        $response = file_get_contents("https://people.googleapis.com/v2/people/me?personFields=names,emailAddresses", false, $context);
+
+        if (!$response) {
+            echo $http_response_header;
+            return;
+        }
+        return json_decode($response, true);
+    }
 }
