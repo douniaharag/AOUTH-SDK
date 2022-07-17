bb074ee5470ad175ac25c3aee2f7bb163401a59a
Dounia HARAG <59885752+douniaharag@users.noreply.github.com>
2022-07-17 22:23:24 +0200

    Version Final ✅ ✅✅

--- client/Providers/GoogleProvider.php
+++ client/Providers/GoogleProvider.php
@@ -56,7 +56,7 @@ class GoogleProvider
             ]
         ]);
 
-        $response = file_get_contents("https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses", false, $context);
+        $response = file_get_contents("https://www.googleapis.com/oauth2/v3/userinfo", false, $context);
 
         if (!$response) {
             echo $http_response_header;
